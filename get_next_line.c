905c5eec6b7fe6a9a6e26ea8a1c20cdc9be76301
Philippe Gobeil-bouchard <pgobeil-@e1z3r1p7.42.us.org>
2019-08-09 13:55:28 -0700

    flipped buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,63 +6,59 @@
 /*   By: pgobeil- <pgobeil-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 17:20:44 by pgobeil-          #+#    #+#             */
-/*   Updated: 2019/06/02 19:54:47 by pgobeil-         ###   ########.fr       */
+/*   Updated: 2019/07/07 00:46:08 by pgobeil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "get_next_line.h"
 
-int		newline(char **s, char **line, int fd, int ret)
+int		clear_buffer(int fd, char **line, char **fds)
 {
-	char	*tmp;
-	int		len;
+	*line = ft_strdup(fds[fd]);
+	ft_strdel(&fds[fd]);
+	return (1);
+}
 
-	len = 0;
-	while (s[fd][len] != '\n' && s[fd][len] != '\0')
-		len++;
-	if (s[fd][len] == '\n')
-	{
-		*line = ft_strsub(s[fd], 0, len);
-		tmp = ft_strdup(s[fd] + len + 1);
-		free(s[fd]);
-		s[fd] = tmp;
-		if (s[fd][0] == '\0')
-			ft_strdel(&s[fd]);
-	}
-	else if (s[fd][len] == '\0')
-	{
-		if (ret == BUFF_SIZE)
-			return (get_next_line(fd, line));
-		*line = ft_strdup(s[fd]);
-		ft_strdel(&s[fd]);
-	}
+int		newline_buffer(int fd, char **line, char **fds)
+{
+	int		i;
+	char	*dump;
+
+	i = 0;
+	while (fds[fd][i] != '\n')
+		i++;
+	*line = ft_strsub(fds[fd], 0, i);
+	dump = ft_strdup(fds[fd] + (i + 1));
+	free(fds[fd]);
+	fds[fd] = dump;
+	if (ft_strlen(fds[fd]) < 1)
+		ft_strdel(&fds[fd]);
 	return (1);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static char			*s[255];
-	char				buff[BUFF_SIZE + 1];
-	char				*tmp;
-	int					ret;
+	static char *fds[MAX_FD];
+	int			read_ret;
+	char		buffer[BUFF_SIZE + 1];
+	char		*dump;
 
-	if (fd < 0 || line == NULL)
-		return (-1);
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	while ((read_ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
-		buff[ret] = '\0';
-		if (s[fd] == NULL)
-			s[fd] = ft_strnew(1);
-		tmp = ft_strjoin(s[fd], buff);
-		free(s[fd]);
-		s[fd] = tmp;
-		if (ft_strchr(buff, '\n'))
-			break ;
+		buffer[read_ret] = '\0';
+		if (fds[fd] == NULL)
+			fds[fd] = ft_strnew(1);
+		dump = ft_strjoin(fds[fd], buffer);
+		free(fds[fd]);
+		fds[fd] = dump;
+		if (ft_strchr(fds[fd], '\n') != NULL)
+			return (newline_buffer(fd, line, fds));
 	}
-	if (ret < 0)
+	if (read_ret < 0 || BUFF_SIZE <= 0 || !line || fd < 0)
 		return (-1);
-	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+	if ((fds[fd] == NULL || fds[fd][0] == '\0') && read_ret == 0)
 		return (0);
-	return (newline(s, line, fd, ret));
+	if (ft_strchr(fds[fd], '\n') != NULL)
+		return (newline_buffer(fd, line, fds));
+	return (clear_buffer(fd, line, fds));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pgobeil- <pgobeil-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 16:02:21 by pgobeil-          #+#    #+#             */
-/*   Updated: 2019/06/02 19:52:20 by pgobeil-         ###   ########.fr       */
+/*   Updated: 2019/07/07 00:46:44 by pgobeil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <fcntl.h>
-# include "libft/libft.h"
+# include "libft/includes/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # define BUFF_SIZE 10
+# define MAX_FD 5000
 
 int		get_next_line(int fd, char **line);
-int		newline(char **s, char **line, int fd, int ret);
+int		clear_buffer(int fd, char **line, char **fds);
+int		newline_buffer(int fd, char **line, char **fds);
 #endif
